cf5a74616d4f22a69d1fd4e8cbbe7b618a2a4779
nobmmeso <blessmmeso4025@gmail.com>
2023-08-29 11:23:45 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * A C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+ *
+ * Use the function printf
+ * You are not allowed to use the function puts
+ * Your program should return 0
+ * Your program should compile without warning when using the -Wall gcc option
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
