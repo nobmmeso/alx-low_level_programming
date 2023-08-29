aa6263c50dd2111c2f10b40b48e67ef1067df3bd
nobmmeso <blessmmeso4025@gmail.com>
2023-08-29 09:56:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * A C program that prints exactly
+ * "Programming is like building a multilingual puzzle, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
